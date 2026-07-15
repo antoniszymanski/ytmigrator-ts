62d2ee3a4261de4a1a04ee162731bd57e3028f0f
Antoni Szymański <antoni.szymanski+github@proton.me>
2026-07-15 14:28:50 +0200

    Remove unused deepEquals import from diff.ts

--- src/sync/diff.ts
+++ src/sync/diff.ts
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Antoni Szymański
 // SPDX-License-Identifier: MPL-2.0
 
-import { deepEquals } from "bun"
 import type { Playlists } from "../remotes"
 
 type PlaylistChange =
